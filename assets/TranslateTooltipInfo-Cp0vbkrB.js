e1717790c16190b849c8bffbeed66a75641f6a9c
EijiGorilla <emasuz@hotmail.com>
2026-01-05 09:25:04 +0800

    Updates

--- assets/TranslateTooltipInfo-Cp0vbkrB.js
+++
@@ -1,1 +0,0 @@
-import{gz as o,X as e,Y as s,$ as r}from"./index-DcRZY5kx.js";import{r as a}from"./rotate-b1Bu2GWE.js";let t=class extends a{constructor(i){super(i),this.type="translate",this.distance=o}clear(){this.distance=o}};e([s()],t.prototype,"type",void 0),e([s()],t.prototype,"distance",void 0),t=e([r("esri.views.interactive.tooltip.infos.TranslateTooltipInfo")],t);export{t as i};
---
+++ assets/highlightOptionsUtils-Biqe1Qlk.js
@@ -0,0 +1,1 @@
+import{cA as n}from"./index-Cf-lFAZt.js";function t(r){return r?.name??n}export{t as r};
