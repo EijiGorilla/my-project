69f3838ebc84fe286774a119086c838fa2add30c
EijiGorilla <emasuz@hotmail.com>
2026-01-05 16:12:00 +0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// type the following in Google
+// How can I avoid publishing _commonHelper file in Vite for github pages?
 export default defineConfig({
   plugins: [react()],
   base: "/my-project/",
+  build: {
+    rollupOptions: {
+      external: ["/_commonjsHelpers-DCkdB7M8.js"],
+    },
+  },
 });
