44a83ed1f77018d8c849fe475d74a2e441e83302
EijiGorilla <emasuz@hotmail.com>
2026-01-05 11:29:33 +0800

    updated

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,45 +1,128 @@
-import "@arcgis/map-components/dist/components/arcgis-map";
-import "@arcgis/map-components/dist/components/arcgis-scene";
-import "@arcgis/map-components/components/arcgis-elevation-profile";
-import "@arcgis/map-components/components/arcgis-direct-line-measurement-3d";
+import { useEffect, useState } from "react";
 import "../index.css";
 import "../App.css";
-import { useEffect, useState } from "react";
-import { ArcgisAreaMeasurement2d } from "@arcgis/map-components/components/arcgis-area-measurement-2d";
+import "@arcgis/map-components/dist/components/arcgis-scene";
+import "@arcgis/map-components/components/arcgis-scene";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "@arcgis/map-components/components/arcgis-legend";
+import "@arcgis/map-components/components/arcgis-basemap-gallery";
+import "@arcgis/map-components/components/arcgis-layer-list";
+import "@arcgis/map-components/components/arcgis-expand";
+import "@arcgis/map-components/components/arcgis-placement";
+import "@arcgis/map-components/components/arcgis-compass";
+import "@arcgis/map-components/components/arcgis-search";
+import {
+  structureLayer,
+  pierAccessLayer,
+  prowLayer,
+  handedOverLotLayer,
+  viaductLayer,
+  utilityGroupLayer,
+  treeGroupLayer,
+  lotGroupLayer,
+  nloLoOccupancyGroupLayer,
+  alignmentGroupLayer,
+  lotLayer,
+  pierHeadColumnLayer,
+} from "../layers";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
+import "@esri/calcite-components/dist/components/calcite-button";
 
-export default function MapDisplay() {
-  const [mapView, setMapView] = useState();
-  const arcgisMap = document.querySelector("arcgis-map");
-  const arcgisElevationProfile = document.querySelector(
-    "arcgis-elevation-profile"
+function MapDisplay() {
+  const [sceneView, setSceneView] = useState();
+  const arcgisScene = document.querySelector("arcgis-scene");
+  const arcgisSearch = document.querySelector("arcgis-search");
+
+  reactiveUtils.when(
+    () => pierHeadColumnLayer.visible === true,
+    () => (viaductLayer.visible = false)
+  );
+
+  reactiveUtils.when(
+    () => viaductLayer.visible === true,
+    () => (pierHeadColumnLayer.visible = false)
+  );
+
+  reactiveUtils.when(
+    () => viaductLayer.visible === false,
+    () => (pierHeadColumnLayer.visible = true)
   );
 
   useEffect(() => {
-    if (mapView) {
-      arcgisElevationProfile.profiles = [
+    if (sceneView) {
+      arcgisScene.map.add(viaductLayer);
+      arcgisScene.map.add(pierAccessLayer);
+      arcgisScene.map.add(utilityGroupLayer);
+      arcgisScene.map.add(treeGroupLayer);
+      arcgisScene.map.add(lotGroupLayer);
+      arcgisScene.map.add(structureLayer);
+      arcgisScene.map.add(nloLoOccupancyGroupLayer);
+      arcgisScene.map.add(alignmentGroupLayer);
+      // arcgisScene.map.add(prowLayer);
+      arcgisScene.map.add(handedOverLotLayer);
+      arcgisScene.map.ground.navigationConstraint = "none";
+      arcgisScene.view.environment.atmosphereEnabled = false;
+      arcgisScene.view.environment.starsEnabled = false;
+
+      arcgisSearch.sources = [
         {
-          type: "ground", // first profile line samples the ground elevation
+          layer: lotLayer,
+          searchFields: ["LotID"],
+          displayField: "LotID",
+          exactMatch: false,
+          outFields: ["LotID"],
+          name: "Lot ID",
+          placeholder: "example: 10083",
         },
         {
-          type: "view", // second profile samples the view and shows building profiles
+          layer: structureLayer,
+          searchFields: ["StrucID"],
+          displayField: "StrucID",
+          exactMatch: false,
+          outFields: ["StrucID"],
+          name: "Structure ID",
+          placeholder: "example: NSRP-01-02-ML007",
+        },
+        {
+          layer: pierAccessLayer,
+          searchFields: ["PierNumber"],
+          displayField: "PierNumber",
+          exactMatch: false,
+          outFields: ["PierNumber"],
+          name: "Pier No",
+          zoomScale: 1000,
+          placeholder: "example: P-288",
         },
       ];
+      arcgisSearch.allPlaceholder = "LotID, StructureID, Chainage";
+      arcgisSearch.includeDefaultSourcesDisabled = true;
+      arcgisSearch.locationDisabled = true;
+      arcgisScene.view.ui.components = [];
     }
   });
 
-  // this is the test apps
   return (
     <arcgis-scene
-      item-id="9a542f6755274436985617a462ffdf44"
-      //   basemap="dark-gray-vector"
-      //   viewingMode="local"
-      //   ground="world-elevation"
+      // item-id="5ba14f5a7db34710897da0ce2d46d55f"
+      basemap="dark-gray-vector"
+      ground="world-elevation"
+      viewingMode="local"
+      zoom="12"
+      center="121.05, 14.4"
       onarcgisViewReadyChange={(event) => {
-        setMapView(event.target);
+        setSceneView(event.target);
       }}
     >
-      <arcgis-elevation-profile slot="bottom-right"></arcgis-elevation-profile>
-      <arcgis-direct-line-measurement-3d slot="bottom-left"></arcgis-direct-line-measurement-3d>
+      <arcgis-compass slot="top-right"></arcgis-compass>
+      <arcgis-expand close-on-esc slot="top-right" mode="floating">
+        <arcgis-search></arcgis-search>
+        {/* <arcgis-placement>
+          <calcite-button>Placeholder</calcite-button>
+        </arcgis-placement> */}
+      </arcgis-expand>
+      <arcgis-zoom slot="bottom-right"></arcgis-zoom>
     </arcgis-scene>
   );
 }
+
+export default MapDisplay;
