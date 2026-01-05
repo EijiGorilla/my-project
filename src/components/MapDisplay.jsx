fbf7ab88edae6cff2a1146a63bf2354f3272b815
EijiGorilla <emasuz@hotmail.com>
2026-01-05 09:42:27 +0800

    updated

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,15 +1,22 @@
 import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-scene";
 import "@arcgis/map-components/components/arcgis-elevation-profile";
+import "@arcgis/map-components/components/arcgis-direct-line-measurement-3d";
 import "../index.css";
 import "../App.css";
 
 export default function MapDisplay() {
   return (
-    <arcgis-map basemap="dark-gray-vector" ground="world-elevation">
+    <arcgis-scene
+      basemap="dark-gray-vector"
+      viewingMode="local"
+      ground="world-elevation"
+    >
       <arcgis-elevation-profile
         slot="bottom-right"
         unit="millimeters"
       ></arcgis-elevation-profile>
-    </arcgis-map>
+      <arcgis-direct-line-measurement-3d slot="bottom-left"></arcgis-direct-line-measurement-3d>
+    </arcgis-scene>
   );
 }
